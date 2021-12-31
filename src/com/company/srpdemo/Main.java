5e62c0a12209b81839844bae517c9a81e2434d6d
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-12-31 13:32:02 -0300

    Single Responsability Principle

---
+++ src/com/company/srpdemo/Main.java
@@ -0,0 +1,22 @@
+package com.company.srpdemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void listClient() {
+
+    }
+
+    public void addClient() {
+
+    }
+    public void updateClient() {
+
+    }
+    public void deleteClient() {
+
+    }
+}
---
+++ src/com/company/srpdemo/utility/Notify.java
@@ -0,0 +1,9 @@
+package com.company.srpdemo.utility;
+
+public class Notify {
+
+    // não inflar uma classe demais
+    public void notifyClient() {
+
+    }
+}
